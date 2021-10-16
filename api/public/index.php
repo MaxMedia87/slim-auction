d394736c447066c6ea4f89bea01b7dcad39a01bc
Maxim <maksim_saharov@mail.ru>
2021-10-16 20:25:24 +0300

    [feat] Рассмотрен базовый пример роутинга запрос/ответ

--- api/public/index.php
+++ api/public/index.php
@@ -2,4 +2,22 @@
 
 declare(strict_types=1);
 
-echo '<h1>Hello World</h1>';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+http_response_code(500);
+
+require __DIR__ . './../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+
+    $response->getBody()->write("Hello $name");
+
+    return $response;
+});
+
+$app->run();
