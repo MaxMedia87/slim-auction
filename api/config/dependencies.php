7ea66fa26e2488861d20c6a0a527413ad61dd95b
Maxim <maksim_saharov@mail.ru>
2021-10-23 16:41:23 +0300

    [feat] Реализация сборщика общего конфига из разных файлов

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -1,8 +1,16 @@
 <?php
 declare(strict_types=1);
 
-return [
-    'config' => [
-        'debug' => (bool)getenv('APP_DEBUG')
-    ],
-];
+$files = array_merge(
+    glob(__DIR__ . '/common/*.php'),
+    glob(__DIR__ . '/' . (getenv('APP_ENV') ?: 'prod') . '/*.php')
+);
+
+$configs = array_map(
+    function ($file) {
+        return require $file;
+    },
+    $files
+);
+
+return array_merge_recursive(...$configs);
